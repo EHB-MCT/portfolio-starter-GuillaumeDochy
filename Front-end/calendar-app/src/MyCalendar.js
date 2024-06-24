69561e3475a3ddf878fdd98626eb4c74c2ac9b9b
Raitro <guillaume.dochy@student.ehb.be>
2024-06-24 12:09:49 +0200

    Correct commit of used and correct files
    This is the correct restructured and clean package of the front-end side of this work. Correct structure and files can be found now with no outdated and used files

---
+++ Front-end/calendar-app/src/MyCalendar.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+import AddEventForm from './AddEventForm';
+import EventDetailsModal from './EventDetailsModal';
+import { fetchEvents, createEvent, updateEvent } from './api';
+import './styles.css';
+
+const localizer = momentLocalizer(moment);
+
+const MyCalendar = ({ onAddEvent, onUpdateEvent }) => {
+  const [events, setEvents] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEventDetailsModalOpen, setIsEventDetailsModalOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedEvents = await fetchEvents();
+      setEvents(fetchedEvents);
+    };
+
+    fetchData();
+  }, []);
+
+  const eventStyleGetter = (event) => {
+    const style = {
+      backgroundColor:
+        event.priority === 'low'
+          ? 'green'
+          : event.priority === 'medium'
+          ? 'orange'
+          : 'red',
+    };
+    return {
+      style,
+    };
+  };
+
+  const handleSelectSlot = (slotInfo) => {
+    setSelectedDate(slotInfo.start);
+    setIsModalOpen(true);
+  };
+
+  const handleAddEvent = async (newEvent) => {
+    const createdEvent = await createEvent(newEvent);
+    setEvents([...events, createdEvent]);
+  };
+
+  const handleSelectEvent = (event) => {
+    setSelectedEvent(event);
+    setIsEventDetailsModalOpen(true);
+  };
+
+  const handleUpdateEvent = async (updatedEvent) => {
+    try {
+      await updateEvent(selectedEvent._id, updatedEvent);
+            window.location.reload();
+    } catch (error) {
+      console.error('Failed to update event:', error);
+    }
+
+    setIsEventDetailsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={handleSelectSlot}>Create Event</button>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        eventPropGetter={eventStyleGetter}
+        style={{ height: 500 }}
+        views={['month']} 
+        onSelectSlot={handleSelectSlot}
+        onSelectEvent={handleSelectEvent}
+      />
+
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={() => setIsModalOpen(false)}>
+          <div className="event-modal" onClick={(e) => e.stopPropagation()}>
+            <AddEventForm selectedDate={selectedDate} onAddEvent={handleAddEvent} />
+          </div>
+        </div>
+      )}
+
+      {isEventDetailsModalOpen && (
+        <EventDetailsModal
+          isOpen={isEventDetailsModalOpen}
+          onRequestClose={() => setIsEventDetailsModalOpen(false)}
+          event={selectedEvent}
+          onAddEvent={handleUpdateEvent}
+        />
+      )}
+    </div>
+  );
+};
+
+export default MyCalendar;
